f1ae848fd63cce1681d9dcda1ad21cfbfe07830c
Katharine Cooney <katharinecooney@gmail.com>
2019-06-16 20:51:29 +0200

    Add hue variable; modify hue variable in our draw function so line color constantly changes

--- script.js
+++ script.js
@@ -8,14 +8,17 @@ canvas.height = window.innerHeight;
 ctx.strokeStyle = '#BADASS';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
+ctx.lineWidth = 20;
 
 let isDrawing = false;
-let lastX = 0;
-let lastY = 0;
+let lastX;
+let lastY;
+let hue = 0;
 
 draw = (event) => {
   if (!isDrawing) return;
   console.log(event);
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
   // begin a path
   ctx.beginPath();
   // specify the path; it must start at an initial (x,y) and end at a final (x,y)
@@ -23,9 +26,18 @@ draw = (event) => {
   ctx.lineTo(event.offsetX, event.offsetY);
   // draw the line
   ctx.stroke();
+  // update the lastx and lastY variables 
+  lastX = event.offsetX;
+  lastY = event.offsetY;
+  hue++;
 }
 
 canvas.addEventListener('mousemove', draw);
-canvas.addEventListener('mousedown', () => isDrawing = true);
+canvas.addEventListener('mousedown', () => {
+  isDrawing = true;
+  // update the lastx and lastY variables 
+  lastX = event.offsetX;
+  lastY = event.offsetY;
+});
 canvas.addEventListener('mouseup', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false);
